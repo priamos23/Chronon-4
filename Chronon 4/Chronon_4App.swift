d225215a511c9fcf962d61d60000eaac5480f468
Stefan Vogt <stefan.vogt@priamos.com>
2023-11-27 17:42:51 +0100

    Initial Commit

---
+++ Chronon 4/Chronon_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Chronon_4App.swift
+//  Chronon 4
+//
+//  Created by Stefan Vogt on 27.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Chronon_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chronon 4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chronon 4
+//
+//  Created by Stefan Vogt on 27.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
